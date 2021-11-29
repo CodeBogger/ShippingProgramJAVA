bb457227a57e24c12930f91f02856fdd9e6aa3d5
Boggers <90660532+CodeBogger@users.noreply.github.com>
2021-11-29 09:10:44 -0500

    Create TextLogic.java
    I made major changes on shipping services and extra fees. Does not seem to work, fees for shipping services aren't counted.

--- TextLogic.java
+++ TextLogic.java
@@ -1,9 +1,9 @@
-package com.company;
+package BeluxShippingPackage;
 import java.util.Scanner;
 
 import java.text.DecimalFormat;
 
-public class TextLogic {
+public class ShippingConfig {
 
     //all important variables to use in shipping
     static double product;
@@ -13,7 +13,7 @@ public class TextLogic {
     static double finalFee = 0.00;
     static int attempt = 0;
     static double weight, distance, ounce, startup, weight1, ounce1, distance1, finalFee1;
-    static String province, item, name, address, shippingServ;
+    static String province, item, name, address, shipping;
     static int channelChoice = 0;
 
 
@@ -78,7 +78,7 @@ public class TextLogic {
             System.out.println("Please enter the cost of your item. No '$' sign: \n");
             product = in.nextDouble();
 
-            finalValueFee = product / 25;
+            finalValueFee = product / 40;
 
             weight = 0;
 
@@ -93,6 +93,9 @@ public class TextLogic {
                 weight1 = in.nextInt();
             } else if (weight1 > 100 || weight1 < 1) {
                 attempt++;
+            }
+            if (weight == 100) {
+            	System.out.println("How big is your package??");
             }
 
 
@@ -125,8 +128,9 @@ public class TextLogic {
             if (distance > 0 && distance1 == .5) {
                 distance1 = distance;
             }
+            Shipping1();
     }
-     public void Shipping1() {
+     public static void Shipping1() {
 
          finalFee1 = fees + finalValueFee;
          String finfee1 = df.format(finalFee1);
@@ -136,7 +140,6 @@ public class TextLogic {
          String decMoney = df.format(finalMoney);
 
          System.out.println("Please type in the service you would like to use: ");
-         System.out.println("Here are all the shipping services: \n\n");
          System.out.println("Here are all the shipping services: \n\n");
          System.out.println("BLMS: Belux Mailing Service");
          System.out.println("BLFS: Belux First Class");
@@ -149,50 +152,82 @@ public class TextLogic {
          System.out.println("BL2D: Belux 2-Day");
          System.out.println("BLA: Belux Air");
          System.out.println("BLON: Belux Overnight \n\n");
+
+         System.out.println("Here are shipping details: \n\n");
+         System.out.println("BLMS: Anything under 8 ounces.");
+         System.out.println("BLFS: Anything ranging from 8 to 16 ounces.");
+         System.out.println("BLLW: Anything ranging from 1 pound to 5 pounds.");
+         System.out.println("BLRS: Anything ranging from 5 pounds to 20 pounds.");
+         System.out.println("BLHW: Anything ranging from 20 to 50 pounds.");
+         System.out.println("BLF: Anything from 50 pounds and 100 pounds.");
+         System.out.println("BLPS: Faster shipping, cost you a little more.");
+         System.out.println("BLSS: Fast shipping with special care, cost you a little more.");
+         System.out.println("BL2D: Extremely fast shipping with a costly fee.");
+         System.out.println("BLA: Will deliver your package in no-time. (Only for rich people).");
+         System.out.println("BLON: Your package will flash before your eyes. (REALLY EXPENSIVE). \n\n");
          System.out.println("If you use 'BLF', 'BLPS', 'BLSS', 'BL2D', 'BLA', 'BLON', you will get charged extra.");
-         System.out.println("[RECOMMENDED]");
-         System.out.println("If you'd like to see details for shipping services please insert 'Switch'.");
-         shippingServ = in.nextLine();
+     
+         shipping = nameScan.nextLine();
 
-         if (shippingServ.equals("BLMS") || shippingServ.equals("BLFS") || shippingServ.equals("BLLW") || shippingServ.equals("BLRS") || shippingServ.equals("BLPS") || shippingServ.equals("BLSS") || shippingServ.equals("BL2D") || shippingServ.equals("BLA") || shippingServ.equals("BLON") || shippingServ.equals("Switch")) {
-             return;
-         } else if(shippingServ.equals("Switch")) {
-             ShippingService();
+         if (shipping.equals("BLMS") || shipping.equals("BLFS") || shipping.equals("BLLW") || shipping.equals("BLRS") || shipping.equals("BLHW") || shipping.equals("BLF") || shipping.equals("BLPS") || shipping.equals("BLSS") || shipping.equals("BL2D") || shipping.equals("BLA") || shipping.equals("BLON")) {
          } else {
-             attempt++;
-             System.out.println("Please try again. \n");
-             SwitchChannel();
+        	 attempt++;
+        	 System.out.println("Please try again.");
+        	 Shipping1();
          }
-
-         if (shippingServ.equals("BLMS") && ounce > 8 && weight > 0 || shippingServ.equals("BLMS") && ounce1 > 8 && weight1 > 0) {
+        	
+         
+       
+         if (shipping.equals("BLMS") && ounce > 8 && weight > 0) {
+        	 if (shipping.equals("BLMS") && ounce1 > 8 && weight1 > 0) {
+        		 if (shipping.equals("BLMS") && ounce > 8 && weight1 > 0) {
+        			 if (shipping.equals("BLMS") && ounce1 > 8 && weight > 0) {
              attempt++;
              System.out.println("Please try again.");
-             String blmsInput = in.nextLine();
-         } else {
-             return;
+             Shipping1();
+        			 }
+        		 }
+        	 }
+         } 
+         if(shipping.equals("BLFS") && ounce < 8 && ounce > 16 && weight > 0) {
+        	 if(shipping.equals("BLFS") && ounce1 < 8 && ounce1 > 16 && weight1 > 0) {
+        		 if(shipping.equals("BLFS") && ounce < 8 && ounce > 16 && weight1 > 0) {
+        			 if(shipping.equals("BLFS") && ounce1 < 8 && ounce1 > 16 && weight > 0) {
+        				 attempt++;
+        				 System.out.println("Please try again.");
+        				 Shipping1();
+        			 }
+        		 }
+        	 }
          }
+        
+            
+         
 
-         if (shippingServ.equals("BLPS")) {
-             finalFee -= 5;
-             finalFee1 -= 5;
-         } else if (shippingServ.equals("BLSS")) {
-             finalFee -= 7;
-             finalFee1 -= 7;
-         } else if (shippingServ.equals("BL2D")) {
-             finalFee -= 10;
-             finalFee -= 10;
-         } else if (shippingServ.equals("BLA")) {
-             finalFee -= 22;
-             finalFee1 -= 22;
-         } else if (shippingServ.equals("BLON")) {
-             finalFee -= 26;
-             finalFee1 -= 26;
-         }
+         if (shipping.equals("BLPS")) {
+             fees = 9;
+             fees = 9;
+         } else if (shipping.equals("BLSS")) {
+             fees = 13;
+             fees = 13;
+         } else if (shipping.equals("BL2D")) {
+             fees = 14;
+             fees = 14;
+         } else if (shipping.equals("BLA")) {
+             fees = 29;
+             fees = 29;
+         } else if (shipping.equals("BLON")) {
+             fees = 39;
+             fees = 39;
+         } else if (shipping.equals("BLF")) {
+        	 fees = 28.25;
+        	 fees = 28.25;
+         } 
 
 
          if (attempt >= 5) {
              System.out.println("We have detected strange activity. Please restart.");
-             SwitchChannel();
+             Shipping();
          }
 
          if (weight1 <= 2) {
@@ -234,41 +269,49 @@ public class TextLogic {
 
          if (weight >= 0 && weight <= 100 && ounce > 1 && ounce <= 16 && distance >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight + "lbs, " + ounce + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight1 >= 0 && weight1 <= 100 && ounce > 1 && ounce <= 16 && distance >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight1 + "lbs, " + ounce + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight >= 0 && weight <= 100 && ounce1 > 1 && ounce1 <= 16 && distance >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight + "lbs, " + ounce1 + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight >= 0 && weight <= 100 && ounce > 1 && ounce <= 16 && distance1 >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight + "lbs, " + ounce + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance1 + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight1 >= 0 && weight1 <= 100 && ounce > 1 && ounce <= 16 && distance1 >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight1 + "lbs, " + ounce + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance1 + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight1 >= 0 && weight1 <= 100 && ounce1 > 1 && ounce1 <= 16 && distance1 >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight1 + "lbs, " + ounce1 + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance1 + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight >= 0 && weight <= 100 && ounce1 > 1 && ounce1 <= 16 && distance1 >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight + "lbs, " + ounce1 + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance1 + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
          if (weight1 >= 0 && weight1 <= 100 && ounce1 > 1 && ounce1 <= 16 && distance >= 1) {
              System.out.println("Shipping '" + item + "' from shipper " + name + ", " + province + ", " + address + ". Weight of the package is " + weight + "lbs, " + ounce1 + " ounces.");
+             System.out.println("Shipping service: "+shipping);
              System.out.println("The total distance to the destination is " + distance1 + " miles.");
              System.out.println("The total fees for your shipping is $" + finfee1 + "\n \n");
          }
@@ -283,6 +326,9 @@ public class TextLogic {
              SwitchChannel();
          } else if (channelChoice == 2) {
              Shipping();
+         }
+         if (channelChoice > 2 || channelChoice < 1) {
+        	 SwitchChannel();
          }
 
          
@@ -317,10 +363,11 @@ public class TextLogic {
         System.out.println("BLSS: Fast shipping with special care, cost you a little more.");
         System.out.println("BL2D: Extremely fast shipping with a costly fee.");
         System.out.println("BLA: Will deliver your package in no-time. (Only for rich people).");
-        System.out.println("BLON: Your package will flash before your eyes. (REALLY EXPENSIVE).");
+        System.out.println("BLON: Your package will flash before your eyes. (REALLY EXPENSIVE). \n\n");
         
-        System.out.println("SCROLL UP TO SEE INFO");
+        System.out.println("[SCROLL UP TO SEE INFO]");
         System.out.println("You are about to switch channels. Insert '1' to switch or insert '2' to keep reviewing shipping services.");
+       
         channelChoice = in.nextInt();
         if (channelChoice == 1) {
             SwitchChannel();
